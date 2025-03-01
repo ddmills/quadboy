91634e2f55058de08a363582bf09623488dd1126
ddmills <mills.dalton@gmail.com>
2025-03-01 11:00:02 -0600

    macroquad

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+fn window_conf() -> Conf {
+    Conf {
+        window_title: "Quadboy".to_string(),
+        window_width: 800,
+        window_height: 600,
+        fullscreen: false,
+        window_resizable: true,
+        ..Default::default()
+    }
+}
+
+#[macroquad::main(window_conf)]
+async fn main() {
+    loop {
+        clear_background(LIGHTGRAY);
+
+        draw_line(40.0, 40.0, 100.0, 200.0, 15.0, BLUE);
+        draw_rectangle(screen_width() / 2.0 - 60.0, 100.0, 120.0, 60.0, GREEN);
+        draw_circle(screen_width() - 30.0, screen_height() - 30.0, 15.0, YELLOW);
+
+        let fps = get_fps();
+        draw_text(fps.to_string().as_str(), 20.0, 20.0, 30.0, DARKGRAY);
+
+        next_frame().await;
+    }
 }
